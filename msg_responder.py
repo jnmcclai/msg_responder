d0312195db18b3dcbb29e7776416a723a5199a00
jnmcclai <jonathan.mcclain@adtran.com>
2016-03-08 08:32:11 -0600

    made some major changes with the layout and functionality
    Added some window panes with label frames and three arguments for qc
    step name, description, and expected results.  Added event to clear some
    'ghost' text upon entry of the text box field.  Plan to retrieve the qc
    name, desc, and actual results and return in xml for interoperability
    with iTest.  Still would be nice to get icon to flash for attention
    (change colors)

--- msg_responder.py
+++ msg_responder.py
@@ -12,7 +12,7 @@ class MsgResponder(Frame):
   
     def __init__(self, parent, text):
         Frame.__init__(self, parent)   
-         
+        self.grid() 
         self.parent = parent
         self.text = text
         self.initUI()
@@ -42,48 +42,57 @@ class MsgResponder(Frame):
 
 
         #building frame
-        frame = Frame(self, relief=RAISED, borderwidth=1)
-        frame.pack(fill=BOTH, expand=True)
+        # frame = Frame(self, relief=RAISED, borderwidth=1)
+        # #frame.grid(row = 0, column = 0, rowspan = 3, columnspan = 2, sticky = W+E+N+S)
+        # frame.pack(fill=BOTH, expand=True)
 
 
-        self.pack(fill=BOTH, expand=True)
+        # self.pack(fill=BOTH, expand=True)
         
-        #adding some widgets
-        label = Label(frame, text = self.text, wraplength = 495, justify = LEFT)
-        label.pack()
-        self.textBox = Text(frame, height = 2, width = 495)
-        self.textBox.pack(side = BOTTOM)
-        #self.textBox.insert(END, '#enter ye comments here')
-        labelBox = Label(frame, text = "Actual Results:")
-        labelBox.pack(anchor = W, side = BOTTOM)
-        passButton = Button(self, text="PASS", command=self.btnClickedPass)
-        passButton.pack(side=RIGHT, padx=5, pady=5)
-        failButton = Button(self, text="FAIL", command=self.btnClickedFail)
-        failButton.pack(side=RIGHT)
+        # #adding some widgets
+        # label = Label(frame, text = self.text, wraplength = 495, justify = LEFT)
+        # label.pack()
+        # self.textBox = Text(frame, height = 2, width = 495)
+        # self.textBox.pack(side = BOTTOM)
+        # #self.textBox.insert(END, '#enter ye comments here')
+        # labelBox = Label(frame, text = "Actual Results:")
+        # labelBox.pack(anchor = W, side = BOTTOM)
+        # passButton = Button(self, text="PASS", command=self.btnClickedPass)
+        # passButton.pack(side=RIGHT, padx=5, pady=5)
+        # failButton = Button(self, text="FAIL", command=self.btnClickedFail)
+        # failButton.pack(side=RIGHT)
 
 
+
+        #ver 02
+        frame1 = Frame(master, bg = 'red')
+        frame1.grid(row = 0, column = 0, rowspan = 3, columnspan = 2, sticky = W+E+N+S)  
+
+root = MsgResponder()
+app = Application(master=root)
+app.mainloop()
 
               
 
-def main(text):
+# def main(text):
   
-    root = Tk()
-    root.geometry("500x300+300+300")
-    app = MsgResponder(root, text)
-    root.mainloop()  
+#     root = Tk()
+#     root.geometry("500x300+300+300")
+#     app = MsgResponder(root, text)
+#     root.mainloop()  
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    #handle command arguments
-    parser = argparse.ArgumentParser(description = 'Message responder - simple GUI to display user defined text and return value based on click of button to pass or fail a test. Returns value PASS=1, FAIL=0')
-    parser.add_argument('text', help = 'text to pass in and display on label')
+#     #handle command arguments
+#     parser = argparse.ArgumentParser(description = 'Message responder - simple GUI to display user defined text and return value based on click of button to pass or fail a test. Returns value PASS=1, FAIL=0')
+#     parser.add_argument('text', help = 'text to pass in and display on label')
     
-    args = parser.parse_args()
+#     args = parser.parse_args()
 
-    if args.text:
-        text = args.text
+#     if args.text:
+#         text = args.text
 
-    #start main
-    print "starting msg responder..."
-    main(text)  
+#     #start main
+#     print "starting msg responder..."
+#     main(text)  
---
+++ msg_responder_ver_02.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python
+#coding: utf-8
+#simple python messenger applications
+
+__author__    = "jnmcclai"
+__copyright__ = "Copyright 2016, Adtran, Inc."
+__version__   = "2.7.7"
+
+import argparse
+from Tkinter import *
+from ttk import Frame, Button, Style
+
+result = 0
+comment = ""
+
+class MsgResponder(Frame):
+  
+    def __init__(self, parent, qc_step_name, qc_step_description, qc_step_expected_results):
+        Frame.__init__(self, parent)   
+        self.parent = parent
+        self.qc_step_name = qc_step_name
+        self.qc_step_description = qc_step_description
+        self.qc_step_expected_results = qc_step_expected_results
+        self.initUI()
+
+    def clearText(self, event):
+        #clear actual results text box event when clicked
+        self.text_qc_results_actual.delete("1.0", END)
+
+    def btnClickedPass(self):
+        #button clicked method - sets var to 1 and gets comments
+        result = 1
+        #get the qc_step_name and qc_step_description as well
+        comment = "<comment>" + self.text_qc_results_actual.get("1.0", END) + "</comment>" 
+        print str(result)
+        print comment
+        self.quit()
+
+    def btnClickedFail(self):
+        #button clicked method - sets var to 0 and gets comments
+        result = 0
+        #get the qc_step_name and qc_step_description as well
+        comment = "<comment>" + self.text_qc_results_actual.get("1.0", END) + "</comment>" 
+        print str(result)
+        print comment
+        self.quit()
+        
+    def initUI(self):
+        
+        #some aesthetic definitions 
+        self.parent.title("Message Responder")
+        self.style = Style()
+        self.style.theme_use("classic")
+
+
+        #building frame
+        frame = Frame(self, relief=RAISED, borderwidth=1)
+        frame.pack(fill=BOTH, expand=True)
+
+
+        self.pack(fill=BOTH, expand=True)
+
+        #create top vertical pane for qc desc
+        pane_top = PanedWindow(frame, orient = VERTICAL)
+        pane_top.pack(fill=BOTH, expand = 1)
+
+        #create bottom panes for the qc expected and actual results fields
+        pane_bottom = PanedWindow(frame)
+        pane_bottom.pack(fill=BOTH, expand = 1)
+
+        #add qc desc label frame to the top pane and a label for the text
+        label_frame_qc_desc = LabelFrame(pane_top, text="QC Description")
+        label_qc_desc = Label(label_frame_qc_desc, text=self.qc_step_description)
+        label_qc_desc.pack(anchor=W)
+        pane_top.add(label_frame_qc_desc)
+
+        #add qc expected results label frame to the bottom pane and a label for the text
+        label_frame_qc_results_expected = LabelFrame(pane_bottom, text="QC Expected Results")
+        label_qc_results_expected = Label(label_frame_qc_results_expected, text=self.qc_step_expected_results, wraplength=500, justify=LEFT)
+        label_qc_results_expected.pack(anchor=W)
+        pane_bottom.add(label_frame_qc_results_expected)
+
+
+        #add qc actual results text box with default text
+        self.text_qc_results_actual = Text(pane_bottom, height=14, width=15)
+        self.text_qc_results_actual.insert(END, "Actual Results...")
+        self.text_qc_results_actual.bind('<FocusIn>', self.clearText)
+        pane_bottom.add(self.text_qc_results_actual) 
+
+        
+        #adding some widgets
+        passButton = Button(self, text="PASS", command=self.btnClickedPass)
+        passButton.pack(side=RIGHT, padx=5, pady=5)
+        failButton = Button(self, text="FAIL", command=self.btnClickedFail)
+        failButton.pack(side=RIGHT)
+
+
+
+              
+
+def main(qc_step_name, qc_step_description, qc_step_expected_results):
+  
+    root = Tk()
+    root.geometry("1024x600+200+100")
+    app = MsgResponder(root, qc_step_name, qc_step_description, qc_step_expected_results)
+    root.mainloop()  
+
+
+if __name__ == '__main__':
+
+    #handle command arguments
+    parser = argparse.ArgumentParser(description = 'Message responder - simple GUI to display user defined text and return value based on click of button to pass or fail a test. Returns value PASS=1, FAIL=0')
+    parser.add_argument('-name', '--qc_step_name', help = 'the QC/ALM step name', default = None)
+    parser.add_argument('-desc', '--qc_step_description', help = 'the QC/ALM step description', default = None)
+    parser.add_argument('-expected', '--qc_step_expected_results', help = 'the QC/ALM step expected results', default = None)
+    
+    
+    args = parser.parse_args()
+
+    if args.qc_step_name:
+        qc_step_name = args.qc_step_name
+    if args.qc_step_description:
+        qc_step_description = args.qc_step_description
+    if args.qc_step_expected_results:
+        qc_step_expected_results = args.qc_step_expected_results
+
+    #start main
+    main(qc_step_name, qc_step_description, qc_step_expected_results)  
